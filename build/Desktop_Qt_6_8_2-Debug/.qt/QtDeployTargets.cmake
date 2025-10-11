ca5ad66889c2342f11beea8f5d1db04dd0b038ed
goldfish9901 <745727212@qq.com>
2025-10-11 16:01:05 +0800

    prune

--- build/Desktop_Qt_6_8_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_MineSweeper_FILE /home/goldfish/ext/other-device-backup/MineSweeper/build/Desktop_Qt_6_8_2-Debug/MineSweeper)
-set(__QT_DEPLOY_TARGET_MineSweeper_TYPE EXECUTABLE)
