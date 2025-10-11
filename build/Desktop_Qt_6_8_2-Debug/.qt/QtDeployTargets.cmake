175d98f8141fcfb9190fd41f294739287095adfc
goldfish9901 <745727212@qq.com>
2025-10-11 15:59:37 +0800

    ignore tmp/: zip analysis dir

---
+++ build/Desktop_Qt_6_8_2-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_MineSweeper_FILE /home/goldfish/ext/other-device-backup/MineSweeper/build/Desktop_Qt_6_8_2-Debug/MineSweeper)
+set(__QT_DEPLOY_TARGET_MineSweeper_TYPE EXECUTABLE)
