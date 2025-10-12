571c31c5b5b9f47b11982d558477a2d4e4e6c6e0
goldfish9901 <745727212@qq.com>
2025-10-12 15:49:21 +0800

    feat(i18n): 添加中文翻译支持
    - 更新 CMakeLists.txt 使用 qt_add_translations 替代旧方法
    - 在 main.cpp 中增强翻译加载的日志输出
    - 新增 MineSweeper_zh_CN.ts 文件包含界面中文翻译- 翻译了包括难度设置、菜单项在内的多个 UI 元素
    - 改进了翻译失败时的调试信息打印

--- MineSweeper_zh_CN.ts
+++ MineSweeper_zh_CN.ts
@@ -1,3 +1,44 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <source>MineSweeper</source>
+      <translation>扫雷</translation>
+    </message>
+    <message>
+      <source>MINESWEEPER</source>
+      <translation>扫雷</translation>
+    </message>
+    <message>
+      <source>EASY</source>
+      <translation>简单</translation>
+    </message>
+    <message>
+      <source>MEDIUM</source>
+      <translation>中等</translation>
+    </message>
+    <message>
+      <source>HARD</source>
+      <translation>困难</translation>
+    </message>
+    <message>
+      <source>CUSTOM</source>
+      <translation>自定义</translation>
+    </message>
+    <message>
+      <source>Game</source>
+      <translation>游戏</translation>
+    </message>
+    <message>
+      <source>Record</source>
+      <translation>记录</translation>
+    </message>
+    <message>
+      <source>Exit</source>
+      <translation>退出</translation>
+    </message>
+  </context>
+</TS>
+
--- main.cpp
+++ main.cpp
@@ -13,10 +13,14 @@ int main(int argc, char *argv[])
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
+        // printf("%s\n",locale.toStdString().c_str());
+        qDebug()<<"found "<<locale;
         const QString baseName = "MineSweeper_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
             a.installTranslator(&translator);
             break;
+        }else{
+            qDebug()<<locale<<" failed";
         }
     }
     mw=new MainWindow();
